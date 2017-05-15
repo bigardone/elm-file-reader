6cd3616dc01a92f5adea1bf3b8235886e43ac007
Ricardo García Vega <bigardone@gmail.com>
2017-05-15 08:45:21 +0200

    basic program

--- src/Main.elm
+++ src/Main.elm
@@ -4,10 +4,64 @@ import Html exposing (..)
 import Native.FileReader
 
 
-main : Html msg
-main =
+-- Model
+
+
+type alias Model =
+    { imageBinary : Maybe String }
+
+
+initialModel : Model
+initialModel =
+    { imageBinary = Nothing }
+
+
+
+-- Messages
+
+
+type Msg
+    = NoOp
+
+
+
+-- Update
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            model ! []
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
     let
         test =
             Native.FileReader.test ()
     in
         text test
+
+
+
+-- Main
+
+
+init : ( Model, Cmd Msg )
+init =
+    initialModel ! []
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = (\_ -> Sub.none)
+        }
